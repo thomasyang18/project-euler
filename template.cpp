93dc2064c5e6d14935b60e0cc3eced7e3451e468
thomasyang18 <tomyangtt18@gmail.com>
2022-09-11 20:52:44 -0400

    M

---
+++ template.cpp
@@ -0,0 +1,34 @@
+#pragma GCC target ("avx2")
+#pragma GCC optimize ("O3")
+#pragma GCC optimize ("unroll-loops")
+
+#include<bits/stdc++.h>
+#include<math.h>
+#include<omp.h>
+
+using namespace std;
+
+typedef long long int ll;
+typedef long double ld;
+typedef pair<ll, ll> pl;
+
+#define K first
+#define V second
+#define G(x) ll x; cin >> x;
+#define GD(x) ld x; cin >> x;
+#define GS(s) string s; cin >> s;
+#define EX(x) { cout << x << '\n'; exit(0); }
+#define A(a) (a).begin(), (a).end()
+
+#define NN
+#define M 1000000007 // 998244353
+
+
+int main(){
+//    freopen("a.in", "r", stdin);
+//    freopen("a.out", "w", stdout);
+
+    ios_base::sync_with_stdio(false); cin.tie(0);
+    cout << fixed << setprecision(20);
+    
+}
